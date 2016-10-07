c388abe0ac295a53d2554a906397c31d8cf17ab0
Dan Witten <dan@danwitten.com>
2016-10-06 22:15:02 -0400

    basic boolean class with truthy string and int values of 0 and 1 accepted

--- src/Boolean.php
+++ src/Boolean.php
@@ -7,5 +7,33 @@ namespace Scalar;
 
 class Boolean
 {
-
+    private $value = false;
+    
+    public function __construct($bool = null)
+    {
+        if (isset($bool)) {
+            try {
+                $this->value = $this->normalize($bool);
+            } catch (\InvalidArgumentException $e) {
+                throw $e;
+            }
+        }
+    }
+    
+    public function getValue() : bool
+    {
+        return $this->value;
+    }
+    
+    private function normalize($bool) : bool
+    {
+        if (is_bool($bool)) {
+            return $bool;
+        } elseif (is_int($bool) && ($bool === 0 || $bool === 1)) {
+            return (bool) $bool;
+        } elseif (is_string($bool) && ($bool === '0' || $bool === '1')) {
+            return (bool) $bool;
+        }
+        throw new \InvalidArgumentException('not a valid boolean');
+    }
 }
--- tests/BooleanTest.php
+++ tests/BooleanTest.php
@@ -10,5 +10,27 @@ class BooleanTest extends PHPUnit_Framework_TestCase
     public function testBooleanDefault()
     {
         $boolean = new Boolean;
+        $this->assertFalse($boolean->getValue());
+    }
+    
+    /**
+     * @dataProvider validBooleanProvider
+     */
+    public function testValidBoolean($valid, $expected)
+    {
+        $bool = new Boolean($valid);
+        $this->assertSame($expected, $bool->getValue());
+    }
+    
+    public function validBooleanProvider()
+    {
+        return array(
+            array(true, true),
+            array(false, false),
+            array(1, true),
+            array(0, false),
+            array('1', true),
+            array('0', false)
+        );
     }
 }
